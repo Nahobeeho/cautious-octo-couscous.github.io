250775e9ef6f81896d7195a426f65061842f20c9
Nahobeeho <ngocthanh021206@gmail.com>
2025-08-05 15:54:12 +0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,14 +1,9 @@
 /*Name this external file gallery.js*/
 
 function upDate(previewPic){
- /* In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
-    
-    2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image 
-    */
-  
+ const imgdiv = document.querySelector(#image);
+imageDiv.style.backgroundImage = `url(${previewImage.src})`;
+	imageDiv.textContent = previewImage.alt;
 	}
 
 	function unDo(){
