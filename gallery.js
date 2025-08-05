efe27e0fd9ef6b56c7184968e4c2c4e818a8f91f
Nahobeeho <ngocthanh021206@gmail.com>
2025-08-05 16:20:05 +0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,15 +1,15 @@
 /*Name this external file gallery.js*/
 
 function upDate(previewPic){
-const imageDiv = document.querySelector("#image");
+  const imageDiv = document.querySelector("#image");
   if (!imageDiv) return;
   imageDiv.style.backgroundImage = `url(${previewPic.src})`;
   imageDiv.textContent = previewPic.alt;
 	}
 
-	function unDo(){
-    const imageDiv = document.querySelector("#image");
-  if (!imageDiv) return;
-  imageDiv.style.backgroundImage = ""; // Or the original CSS background
+function unDo(){
+const imageDiv = document.querySelector("#image");
+if (!imageDiv) return;
+imageDiv.style.backgroundImage = ""; // Or the original CSS background
   imageDiv.textContent = "Hover over an image below to display here.";
 	}
