5ea7d72db93008c9791fde29c5464f96848cb26b
Nahobeeho <ngocthanh021206@gmail.com>
2025-08-05 15:56:20 +0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -7,12 +7,7 @@ imageDiv.style.backgroundImage = `url(${previewImage.src})`;
 	}
 
 	function unDo(){
-     /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    */
-		
+    const imageDiv = document.querySelector("#image");
+	imageDiv.style.backgroundImage = "url('original-image.jpg')";
+		imageDiv.textContent = "Original Text";
 	}
